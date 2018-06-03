aa1ae435ef90fb2ca67e3b0b046ec1e0b87d5e71
Jolyn Wong <shiroyami08@gmail.com>
2018-06-03 12:58:19 +0800

    player name

--- Photon Vikings/Assets/DemoVikings/Scripts/PlayerName.cs
+++ Photon Vikings/Assets/DemoVikings/Scripts/PlayerName.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 /* This is for Player Name */
@@ -8,22 +9,41 @@ public class PlayerName : MonoBehaviour
     [SerializeField]
     private TextMesh m_text;
 
+    //private string m_playerName;
+
 	// Use this for initialization
 	void Start ()
     {
-        //m_text.color = Color.red;
+        /* Add to method to carry out event */
+        //PhotonNetwork.OnEventCall += this.OnEventHandler;
+        //m_playerName = "";
         PrintName();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-    // Printing Player Name
-    public string PrintName()
+    /* Print name of player */
+    private string PrintName()
     {
-        return m_text.text = PhotonNetwork.playerName;
+        foreach (PhotonPlayer _player in PhotonNetwork.playerList)
+        {
+            return m_text.text = _player.NickName;
+        }
+
+        return null;
+
+           //return m_text.text = PhotonNetwork.playerName;
     }
 
+    ///* Receive data of events from server */
+    //private void OnEventHandler(byte eventCode, object content, int senderId)
+    //{
+    //    switch (eventCode)
+    //    {
+    //        case 2:
+    //            m_playerName = (string)content;
+    //            Debug.Log("Name : " + m_playerName);
+    //            //Debug.Log(string.Format("Message from Server : {0}", (string)content));
+    //            break;
+    //    }
+    //}
+    
 }
