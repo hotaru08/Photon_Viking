aaad12bf83fbd184354794e72cb234a1e5981ddd
Jolyn Wong <shiroyami08@gmail.com>
2018-06-03 22:30:56 +0800

    spent 1 day to only figure out that 1 line of code is needed only

--- Photon Vikings/Assets/DemoVikings/Scripts/PlayerName.cs
+++ Photon Vikings/Assets/DemoVikings/Scripts/PlayerName.cs
@@ -9,11 +9,11 @@ public class PlayerName : MonoBehaviour
     [SerializeField]
     private TextMesh m_text;
     
-    private static GameObject m_player;
-    public void SetPlayer(GameObject _player)
-    {
-        m_player = _player;
-    }
+    //private static GameObject m_player;
+    //public void SetPlayer(GameObject _player)
+    //{
+    //    m_player = _player;
+    //}
 
     //private string m_playerName;
 
@@ -23,33 +23,33 @@ public class PlayerName : MonoBehaviour
         /* Add to method to carry out event */
         //PhotonNetwork.OnEventCall += this.OnEventHandler;
         //m_playerName = "";
-        //m_text.text = GetComponentInParent<PhotonView>().owner.NickName;
+        m_text.text = GetComponentInParent<PhotonView>().owner.NickName;
         //PrintName();
     }
 
     /* Print name of players */
-    private void PrintName()
-    {
-        //if(PhotonNetwork.playerName == PlayerPrefs.GetString("playerName"))
-        //m_text.text = PlayerPrefs.GetString("playerName"); // return player's name
+    //private void PrintName()
+    //{
+    //    //if(PhotonNetwork.playerName == PlayerPrefs.GetString("playerName"))
+    //    //m_text.text = PlayerPrefs.GetString("playerName"); // return player's name
 
-        //Debug.Log(PhotonNetwork.player.ID + ", " + GetComponentInParent<PhotonView>().ownerId);
+    //    //Debug.Log(PhotonNetwork.player.ID + ", " + GetComponentInParent<PhotonView>().ownerId);
 
-        if (GetComponentInParent<PhotonView>().isMine)
-        {
-            m_text.text = PhotonNetwork.playerName; // return player's name
-            Debug.Log("This is player Name : " + m_text.text);
-        }
-        else if (!GetComponentInParent<PhotonView>().isMine)
-        {
-            // for each player in the room 
-            foreach (var _player in PhotonNetwork.otherPlayers)
-            {
-                m_text.text = _player.NickName; //return others names
-                Debug.Log("This is other Name : " + m_text.text);
-            }
-        }
-    }
+    //    if (GetComponentInParent<PhotonView>().isMine)
+    //    {
+    //        m_text.text = PhotonNetwork.playerName; // return player's name
+    //        Debug.Log("This is player Name : " + m_text.text);
+    //    }
+    //    else if (!GetComponentInParent<PhotonView>().isMine)
+    //    {
+    //        // for each player in the room 
+    //        foreach (var _player in PhotonNetwork.otherPlayers)
+    //        {
+    //            m_text.text = _player.NickName; //return others names
+    //            Debug.Log("This is other Name : " + m_text.text);
+    //        }
+    //    }
+    //}
 
     ///* Receive data of events from server */
     //private void OnEventHandler(byte eventCode, object content, int senderId)
