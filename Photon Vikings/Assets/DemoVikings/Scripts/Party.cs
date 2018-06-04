f14c00bdf05d49fdebcd5134fc47506f5c4598c1
Marcus- <marcuslim4299@gmail.com>
2018-06-04 14:39:03 +0800

    sry

---
+++ Photon Vikings/Assets/DemoVikings/Scripts/Party.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Party : MonoBehaviour {
+
+    public string[] members;
+	// Use this for initialization
+	void Start () {
+        members = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        members = GameObject.FindGameObjectWithTag("Code").GetComponent<ChatVik>().Party;
+	}
+}
